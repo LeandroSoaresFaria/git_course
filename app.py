9674ac100dea25f6d27be5465865cb707d374692
LeandroSoaresFaria <leandro_faria@yahoo.com.br>
2023-03-18 18:05:36 -0300

    add main function

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import streamlit as st
 def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
+def main():
+    df = load_data()
 
-st.dataframe()
+    st.dataframe(df)
+
+if __name__ == '__main__'
+
+   main()
