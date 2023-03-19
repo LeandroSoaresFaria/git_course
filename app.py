6e2140c8480cc42d15d4a5c9e7899d5a68fcfc71
LeandroSoaresFaria <leandro_faria@yahoo.com.br>
2023-03-19 09:02:40 -0300

    fixed app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from src.extration import load_data
 import numpy as np
 
-import streamlit as st
+#:import streamlit as st
 
-st.set_page_config(layout = wide)
+#st.set_page_config(layout = wide)
 
 def main():
     df_raw = load_data()
 
-    st.dataframe(df_raw)
+    #st.dataframe(df_raw)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 
    main()
